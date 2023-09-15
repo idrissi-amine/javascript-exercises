a49acb6bcbc9b57701796a810a11ecdf1eae449a
amineidrissi <amineidrissis12@gmail.com>
2023-09-15 01:16:35 +0100

    added shiit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(word, amount) {
+    if(amount<0){
+        return "ERROR"
+    }
+    let res= "";
+    for(let i =0; i<amount;i++){
+        res = res + word
+    }
+    return res;
 
 };
 
