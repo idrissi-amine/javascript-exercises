e75c1fad48f1b8d3d565b987d513547ced9bedae
amineidrissi <amineidrissis12@gmail.com>
2023-09-16 00:40:39 +0100

    calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,42 @@
-const add = function() {
+const add = function(a, b) {
+  return a+b
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b
 	
 };
 
-const sum = function() {
+const sum = function(e) {
+  let total = 0
+  for(let i = 0 ; i < e.length ; i++){
+    total = total + e[i]
+  }
+  return total
 	
 };
 
-const multiply = function() {
+const multiply = function(e) {
+  let total = 1
+  for(let i = 0 ; i < e.length ; i++){
+    total = total * e[i]
+  }
+  return total
 
 };
 
-const power = function() {
+const power = function(a, b) {
+  return a ** b
 	
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  if(a == 0){
+    return 1
+  }else{
+    return a*factorial(a-1)
+  }
 	
 };
 
