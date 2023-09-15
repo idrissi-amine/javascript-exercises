476a7430750cf924daeade102f594d81356496ea
amineidrissi <amineidrissis12@gmail.com>
2023-09-15 01:30:10 +0100

    added second exo

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let res = "";
+    for (let i = 1; i <= word.length; i++) {
+        res = res + word[word.length - i]
+        
+    }
+    return res 
+
 
 };
 
