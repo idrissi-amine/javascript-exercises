32931fabf7f36d2922b56991579f7a1e13c9af3c
amineidrissi <amineidrissis12@gmail.com>
2023-09-15 02:02:43 +0100

    added sumAll

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, args) {
+  if (args.length == undefined) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == args) {
+        array.splice(i, 1);
+      }
+    }
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < args.length; j++) {
+        if (array[i] == args[j]) {
+          array.splice(i, 1);
+        }
+      }
+    }
+  }
 
+  return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(begin, end) {
+    let res = 0;
+    if(begin <0 || end <0 || typeof(begin) != "number" || typeof(end) != 'number'){
+        return "ERROR"
+    }
+    else if(begin < end){
+        for (let i = begin; i <= end; i++) {
+        res = res + i;
+        
+    }
+    }else{
+        for (let i = end; i <= begin; i++) {
+            res = res + i;
+            
+        }
+    }
+    
+    return res
 
 };
 
